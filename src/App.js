dfe63497187abbe09cde5ceedcb9862b6966ee45
Rysth <johnpalacios.t@gmail.com>
2023-09-13 13:10:54 -0500

    Prepared general styling and tailwind.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="app">
+      <div className="text-center bg-black text-white text-4xl">Hello World!</div>
+    </div>
+  );
 }
 
 export default App;
